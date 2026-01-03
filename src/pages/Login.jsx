605fc3332b607ca4fa5ee93e99ca158d02f5a62a
MustafaZoubi <220932775+MustafaZoubi@users.noreply.github.com>
2026-01-03 13:54:43 +0300

    Finish Login form with responsive applied

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,59 @@
 import React from 'react'
-
+import backVideo from "../assets/images/backVideoLogin.mp4"
+import logo from "../assets/images/BerserkLogo.png"
+import style from "../styles/login.module.css"
+import { MdEmail } from "react-icons/md";
+import { Link } from 'react-router'
 export default function Login() {
     return (
-        <div>Login</div>
+
+        <div className={style.mainContainer}>
+            <video
+                className={style.backVideo}
+                src={backVideo}
+                autoPlay
+                loop
+                muted>
+            </video>
+            <div className={style.loginContainer}>
+                <img className={style.logo} src={logo} />
+                <div className={style.innerContainer}>
+                    <div className={style.emailContainer}>
+                        <label htmlFor='email'>Email Address</label>
+                        <input id='email' className={style.loginField} type="email" placeholder='you@example.com' />
+                    </div>
+                    <div className={style.passContainer}>
+                        <label htmlFor='password'>Password</label>
+                        <input id='password' className={style.loginField} type="password" placeholder='Enter your password' />
+                    </div>
+                    <div className={style.tools}>
+                        <div className={style.remember}>
+                            <input id='checkbox' type='checkbox' />
+                            <label htmlFor='checkbox'>Remember me</label>
+                        </div>
+
+                        <Link target='_blank' to="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> <p className={style.forgotPass}>Forgot password?</p></Link>
+                    </div>
+                    <button className={style.loginBtn}>Sign In</button>
+                    <div className={style.accountDetails}>
+                        <p className={style.demo}>Demo Credentials</p>
+                        <p className={style.details}>
+                            <p>User:</p>
+                            <p>user@arcadia.com</p>
+                        </p>
+                        <p className={style.details}>
+                            <p>Admin:</p>
+                            <p>admin@arcadia.com</p>
+                        </p>
+                        <p className={style.details}>
+                            <p>Password:</p>
+                            <p>Any password</p>
+                        </p>
+                    </div>
+
+                    <p className={style.account}>Don't have an account? <Link to="signup">Sign up for free</Link></p>
+                </div>
+            </div>
+        </div>
     )
 }
