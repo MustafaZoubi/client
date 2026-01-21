84cd197cece03a1c6d1c593712108946095ac9fc
MustafaZoubi <220932775+MustafaZoubi@users.noreply.github.com>
2026-01-22 01:34:07 +0300

    fixing things

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
-import style from "../styles/card.module.css"
-import windows from "../assets/images/icons/windows.png"
-import playstation from "../assets/images/icons/playstation.png"
-import xbox from "../assets/images/icons/xbox.png"
-import { Link } from "react-router"
+import React from "react";
+import style from "../styles/card.module.css";
+import windows from "../assets/images/icons/windows.png";
+import playstation from "../assets/images/icons/playstation.png";
+import xbox from "../assets/images/icons/xbox.png";
+import { Link } from "react-router";
 
-export default function Card() {
+export default function Card({ game }) {
     return (
-        <Link to="details/1">
+        <Link to={`details/${game._id}`}>
             <div className={style.card}>
-                <img src='https://media.rawg.io/media/games/526/526881e0f5f8c1550e51df3801f96ea3.jpg' />
+                <img src={game.coverImage} />
+
                 <div className={style.bottomSection}>
                     <div className={style.cost}>
                         <p>Add to cart +</p>
-                        <p>$12.98</p>
+                        <p>${game.price.toFixed(2)}</p>
                     </div>
-                    <div className={style.icons}>
-                        <img className={style.icon} src={windows} />
-                        <img className={style.icon} src={playstation} />
-                        <img className={style.icon} src={xbox} />
-                    </div>
-                    <p className={style.title}>
-                        Vampire: The Masquerade – Bloodlines 2
-                    </p>
-                </div>
 
+                    <div className={style.icons}>
+                        {game.platforms?.pc && <img src={windows} />}
+                        {game.platforms?.playstation && <img src={playstation} />}
+                        {game.platforms?.xbox && <img src={xbox} />}
+                    </div>
+
+                    <p className={style.title}>{game.title}</p>
+                </div>
             </div>
         </Link>
-
-
-    )
+    );
 }
--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -1,42 +1,33 @@
-import React, { useState } from 'react'
-import Card from "../components/Card.jsx"
-import Navbar from "../components/Navbar.jsx"
-import CategoriesTab from '../components/CategoriesTab.jsx'
-import style from "../styles/browsepage.module.css"
-import { GiHamburgerMenu } from "react-icons/gi";
-import { IoCloseSharp } from "react-icons/io5";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card.jsx";
+import Navbar from "../components/Navbar.jsx";
+import CategoriesTab from "../components/CategoriesTab.jsx";
+import style from "../styles/browsepage.module.css";
+import { fetchGames } from "../api/gameApi";
 
 function BrowsePage() {
-    const [mobNav, setMobNav] = useState(false);
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetchGames().then(setGames).catch(console.error);
+    }, []);
 
     return (
         <div className={style.mainContainer}>
             <Navbar backgroundOn={true} />
             <div className={style.divider}></div>
+
             <div className={style.middleContainer}>
                 <CategoriesTab className={style.leftNav} />
+
                 <div className={style.gamesContainer}>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    {games.map((game) => (
+                        <Card key={game._id} game={game} />
+                    ))}
                 </div>
             </div>
-            <div onClick={() => setMobNav(!mobNav)} className={style.burgerMenu}>
-                {mobNav ? (<IoCloseSharp />) : (<GiHamburgerMenu />)}
-            </div>
-
-            <CategoriesTab className={`${style.mobileNav} ${mobNav ? style.active : null}`} />
-
         </div>
-
-
-
-
-
-
-    )
+    );
 }
 
-export default BrowsePage
+export default BrowsePage;
