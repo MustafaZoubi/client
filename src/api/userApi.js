bbccf73346d979896899d59c8dd0fff2f7270b87
MustafaZoubi <220932775+MustafaZoubi@users.noreply.github.com>
2026-01-22 00:36:26 +0300

    Update the API to handle editing profile

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -11,3 +11,18 @@ export const fetchProfile = async (token) => {
     if (!res.ok) throw new Error(data.message);
     return data;
 };
+
+export const updateProfileApi = async (token, payload) => {
+    const res = await fetch(`${BASE_URL}/profile`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(payload),
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message);
+    return data;
+};
